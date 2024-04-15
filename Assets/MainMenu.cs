570dfd68a2a0e58567b24efcabb5d268e69d6100
michelfournier <michel.fournier.trad@gmail.com>
2024-04-16 00:40:47 +0200

    added main menu and play stage for cpu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Playgame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+    
+    public void PlayCPU()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void Quitgame()
+    {
+        Application.Quit();
+        Debug.Log("Quitting");
+    }
+
+}
