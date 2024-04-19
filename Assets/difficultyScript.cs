c835d3a64e33f672dafa0392be58ff5083e7f92c
michelfournier <michel.fournier.trad@gmail.com>
2024-04-19 15:36:29 +0200

    added game over to 1vs1 and changed speed rating for difficulties

--- Assets/difficultyScript.cs
+++ Assets/difficultyScript.cs
@@ -13,7 +13,7 @@ public class difficultyScript : MonoBehaviour
     {
         logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();    
 
-        logic.ballRate = 4f;
+        logic.ballRate = 5f;
         logic.p1Rate = 3f;
         logic.p2Rate = 3f;  
         
@@ -22,19 +22,19 @@ public class difficultyScript : MonoBehaviour
     {
         if (val == 0)
         {
-            logic.ballRate = 4f;
+            logic.ballRate = 5f;
             logic.p1Rate = 3f;
             logic.p2Rate = 3f;
         }
         if (val == 1)
         {
-            logic.ballRate = 5f;
+            logic.ballRate = 6f;
             logic.p1Rate = 4f;
             logic.p2Rate = 4f;
         }
         if (val == 2)
         {
-            logic.ballRate = 6f;
+            logic.ballRate = 7f;
             logic.p1Rate = 5f;
             logic.p2Rate = 5f;
         }
